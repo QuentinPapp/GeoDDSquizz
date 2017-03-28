c33d2de54b6070555960f2bd6653408d85fa76be
Yohko Boz <yoyohko@gmail.com>
2017-03-28 16:45:11 +0200

    mise a jour tuto citations react

--- react/src/index.js
+++ react/src/index.js
@@ -1,1 +1,34 @@
-alert('KMI KOKAR');
+import React from 'react';
+import ReactDom from 'react-dom';
+import citations from './citations';
+import './index.css';
+
+class App extends React.Component {
+
+	state = {};
+
+	genererCitation = event => {
+		// On transforme les citations en array
+		const keyArray = Object.keys(citations);
+		// Une citation au hasard
+		const randomKey = keyArray[Math.floor(Math.random() * keyArray.length)];
+		this.setState(citations[randomKey]);
+	};
+
+	render() {
+		return (
+			<div>
+				<p>
+					{this.state.citation}
+					<span>- {this.state.auteur}</span>
+				</p>
+				<button onClick={e => this.genererCitation(e)}>Une autre citation!</button>
+			</div>
+		)
+	}
+}
+
+ReactDom.render(
+	<App />,
+	document.getElementById('root')
+);
