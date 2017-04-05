d7fb84a5e768bc9ec4648377180e03e28a1c9ff6
QuentinPapp <Quentin.papp@outlook.fr>
2017-04-05 11:38:50 +0200

    Optimisation de la requete SQL

--- moulinette.py
+++ moulinette.py
@@ -7,41 +7,55 @@ import json
 from random import randrange
 import psycopg2
 
+
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+
+# def departement():
+# 	try:
+# 		dbcon = psycopg2.connect('host=vps338664.ovh.net port=5432 dbname=geoquizz user=geodds password=acsdds')
+# 		dbcur = dbcon.cursor()
+# 		dbcur.execute("SELECT nom_departement FROM test ORDER BY random() LIMIT 20")
+# 		departements = dbcur.fetchall()
+# 	except:
+# 		return "département() ne fonctionne pas"
+
+# def mauvrep():
+	# def qui genère des réponse aléatoire en fonction de la colonne de la question
+
+def moulinette():
 	try:
 		dbcon = psycopg2.connect('host=vps338664.ovh.net port=5432 dbname=geoquizz user=geodds password=acsdds')
-		dbcur = dbcon.cursor() 
-		dbcur.execute("SELECT * FROM test ORDER BY random() LIMIT 20")
+		dbcur = dbcon.cursor()
+		dbcur.execute("CREATE TEMP TABLE departement AS SELECT DISTINCT nom_region,chef_lieu,num_departement,nom_departement,prefecture FROM test")
+		dbcur.execute("SELECT nom_region,chef_lieu,num_departement,nom_departement,prefecture FROM departement ORDER BY random() LIMIT 20")
 		datas = dbcur.fetchall()
 		i = randrange(8)
 		moulinette = []
 		for i in range(0,len(datas)): # i est compris entre 0 et 20
 			try:
 				for data in datas:
-					questions = [('Quel est le numéro du département ' + data[3] + ' ?'),
-					('Quel est le département qui à pour numéro ' + data[4] + ' ?'),
-					('Quelle est la préfecture du département ' + data[4] + ' ?'),
-					(data[5] + ' est la préfecture de quel département ?'),
-					('Dans quel département se trouve la préfecture de ' + data[5] + ' ?'),
-					('Dans quelle région se trouve le département ' + data[4] + ' ?'),
-					(data[4] + ' est un département. Quand quelle région se trouve-t-il ?'),
-					('Quel est le chef-lieu de la région' + data[1] + '?')]
-					moulinette.append(questions[i])
+					modeles = { 
+					0 : ('Quel est le numéro du département ' + data[3] + ' ?', str(data[2])),
+					1 : ('Quel est le département qui a pour numéro ' + data[3] + ' ?', str(data[2])),
+					2 : ('Quelle est la préfecture du département ' + data[3] + ' ?', str(data[4])),
+					3 : (data[4] + ' est la préfecture de quel département ?', str(data[3])),
+					4 : ('Dans quel département se trouve la préfecture de ' + data[4] + ' ?', str(data[3])),
+					5 : ('Dans quelle région se trouve le département ' + data[3] + ' ?', str(data[0])),
+					6 : (data[3] + ' est un département. Dans quelle région se trouve-t-il ?', str(data[0])),
+					7 : ('Quel est le chef-lieu de la région' + data[0] + '?', str(data[1]))
+					}
+					moulinette.append(modeles[i])
 					i = randrange(8)
-
-				return json.dumps(moulinette, indent = 4)
-		#pou question[0] 'Quel est le numéro du département ' + data[3] + '?' on prend le 1er élément de datas 
-		# on prend ensuite question[1]
-		# on passe au 2 élément de datas
+				return str(moulinette)
+				#On utilise la méthode dump pour enregistrer l'objet.
 			except:
 				return "La fonction for ne fonctionne pas"
 	except:
 		return " connexion au serveur a échoué" 
 
-app.run(host='0.0.0.0', port=11001)
+app.run(host='0.0.0.0', port=11002)
 
 
 
