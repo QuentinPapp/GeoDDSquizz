9e77eab1b4e920dc4d39200dd77f861d67a54fbf
Yohko Boz <yoyohko@gmail.com>
2017-04-21 09:54:23 +0200

    affichage fin de partie

--- src/components/Body/Question.js
+++ src/components/Body/Question.js
@@ -5,12 +5,14 @@ export default class Question extends React.Component {
 
 	render() {
 		return (
-			<section className="content question">
-				<h5>{this.props.question}</h5>
-				<label htmlFor="repA" tabIndex="1"><input type="radio" id="repA" className="rep" name="choice" value={this.props.propositions[0]} onClick={this.props.checkRep} /> {this.props.propositions[0]}</label>
-				<label htmlFor="repB" tabIndex="2"><input type="radio" id="repB" className="rep" name="choice" value={this.props.propositions[1]} onClick={this.props.checkRep} /> {this.props.propositions[1]}</label>
-				<label htmlFor="repC" tabIndex="3"><input type="radio" id="repC" className="rep" name="choice" value={this.props.propositions[2]} onClick={this.props.checkRep} /> {this.props.propositions[2]}</label>
-			</section>
+			<div id="ask">
+				<section className="content question">
+					<h5>{this.props.question}</h5>
+					<label htmlFor="repA"><input type="radio" id="repA" className="rep" name="choice" value={this.props.propositions[0]} onClick={this.props.checkRep} /> {this.props.propositions[0]}</label>
+					<label htmlFor="repB"><input type="radio" id="repB" className="rep" name="choice" value={this.props.propositions[1]} onClick={this.props.checkRep} /> {this.props.propositions[1]}</label>
+					<label htmlFor="repC"><input type="radio" id="repC" className="rep" name="choice" value={this.props.propositions[2]} onClick={this.props.checkRep} /> {this.props.propositions[2]}</label>
+				</section>
+			</div>
 		);
 	}
 }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -43,7 +43,7 @@ export default class Layout extends React.Component {
 
 	checkRep() {
 
-		if (this.state.indice <= 19) {
+		
 			var form = new FormData();
 			form.append("question", this.state.indice);
 			form.append("reponse", document.querySelector('input[name="choice"]:checked').value);
@@ -61,42 +61,65 @@ export default class Layout extends React.Component {
 			})
 			.then((res) => {
 			  if (res[0].resultat === "True") {
-			  	var ok = document.createElement("img");
-			  	var p1 = document.getElementById("test").firstChild;
-			  	ok.className = "show";
-			  	ok.src = "http://nabilb.dijon.codeur.online/geoquizz/ressources/check.png";
-			  	document.getElementById("test").replaceChild(ok,p1);
-			  	setTimeout(() => {
-			  		this.setState({indice : this.state.indice + 1});
-			  		this.setState({question : this.state.data[this.state.indice].question});
-					this.setState({propositions: this.state.data[this.state.indice].propositions});
-					document.getElementById("test").replaceChild(p1,ok);
-					document.querySelector('input:checked').checked = false;
-			  	}, 2000);
+
+			  	var rep_ok  = document.querySelector('input[name="choice"]:checked').parentNode;
+				rep_ok.classList.add("ok");
+
+				this.setState({indice : this.state.indice + 1});
+
+				if (this.state.indice <= 18) {
+				  	setTimeout(() => {
+				  		this.setState({question : this.state.data[this.state.indice].question});
+						this.setState({propositions: this.state.data[this.state.indice].propositions});
+						rep_ok.classList.remove("ok");
+						document.querySelector('input:checked').checked = false;
+				  	}, 1000);
+			  	} else {
+			  		var end_ok = document.createElement("h3");
+					var txt_ok = document.createTextNode("Partie terminée !!");
+
+					end_ok.appendChild(txt_ok);
+
+					var q_ok = document.getElementById("ask").firstChild;
+
+					document.getElementById("ask").replaceChild(end_ok,q_ok);
+					setTimeout(() => {
+						window.location.reload();
+					}, 4000);
+			  	}
 
 			  	this.setState({score: res[0].score});
 			  }	else {
-			  		var nok = document.createElement("img");
-				  	var p2 = document.getElementById("test").firstChild;
-				  	nok.className = "show";
-				  	nok.src = "http://nabilb.dijon.codeur.online/geoquizz/ressources/croix.png";
-				  	document.getElementById("test").replaceChild(nok,p2);
+				  	if (this.state.indice <= 18) {
+					  	var rep_nok  = document.querySelector('input[name="choice"]:checked').parentNode;
+					  	rep_nok.classList.add("nok");
 
-				  	setTimeout(() => {
-			  		this.setState({indice : this.state.indice + 1});
-			  		this.setState({question : this.state.data[this.state.indice].question});
-					this.setState({propositions: this.state.data[this.state.indice].propositions});
-					document.getElementById("test").replaceChild(p2,nok);
-					document.querySelector('input:checked').checked = false;
-			  	}, 2000);
+					  	this.setState({indice : this.state.indice + 1});
+
+					  	setTimeout(() => {
+					  		this.setState({question : this.state.data[this.state.indice].question});
+							this.setState({propositions: this.state.data[this.state.indice].propositions});
+							rep_nok.classList.remove("nok");
+							document.querySelector('input:checked').checked = false;
+					  	}, 1000);
+				  	} else {
+				  		var end_nok = document.createElement("h3");
+						var txt_nok = document.createTextNode("Partie terminée !!");
+
+						end_nok.appendChild(txt_nok);
+
+						var q_nok = document.getElementById("ask").firstChild;
+
+						document.getElementById("ask").replaceChild(end_nok,q_nok);
+						setTimeout(() => {
+							window.location.reload();
+						}, 4000);
+			  		}	
 			  	}
 			})	
 			.catch(function (error) {  
 			  console.log("nok", error);
 			});
-		} else {
-			console.log("end");
-		}
 	}
 
 	render() {
